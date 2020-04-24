73dcf439f77b13a9c9dd742781b274f18e5b169c
sridharyadav <38374318+sridharyadav@users.noreply.github.com>
2020-04-24 09:12:12 +0530

    Update productController.java

--- DrinksAndDelightComplete/src/main/java/com/capgemini/main/controller/productController.java
+++ DrinksAndDelightComplete/src/main/java/com/capgemini/main/controller/productController.java
@@ -5,16 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-import com.capgemini.main.entites.RawSpecs;
+import com.capgemini.main.entites.StockDetails;
 import com.capgemini.main.service.ProductServiceInterface;
 
 @RestController
@@ -31,18 +30,22 @@ public class productController {
 	
 	
 	@GetMapping("/getProductSpecs/{id}")
-	public ResponseEntity<RawSpecs> getProductSpecs(@PathVariable String id)
+	public ResponseEntity<StockDetails> getProductSpecs(@PathVariable String id)
 	{
-		RawSpecs list = serviceObj.getProductSpecs(id);
-		return new ResponseEntity<RawSpecs>(list,HttpStatus.OK);
+		StockDetails list = serviceObj.getProductSpecs(id);
+		return new ResponseEntity<StockDetails>(list,HttpStatus.OK);
 	}
 	
+	
+	
 	@GetMapping("/getAllProductSpecs")
-    public ResponseEntity<List<RawSpecs>> getProductList() {
-			List<RawSpecs> list = serviceObj.retrieve();
-			System.out.println("hi");
-			return new ResponseEntity<List<RawSpecs>>(list,HttpStatus.OK);
+    public ResponseEntity<List<StockDetails>> getProductList() {
+			List<StockDetails> list = serviceObj.retrieve();
+			
+			return new ResponseEntity<List<StockDetails>>(list,HttpStatus.OK);
 	}
+	
+	
 	
 	
 }
